2a2f3f21b8d7cd4d6d5d3460788ad2070e3bdca6
Max Carroll <max.carroll@barbal.co>
2021-10-13 15:30:25 +0100

    stubbed out an iocContainer class - will now start doing some TDD

---
+++ src/lib/iocContainer.ts
@@ -0,0 +1,12 @@
+/**
+ * TODO: Add docs - will TDD this first
+ *
+ * Use the iocContainer class to register and resolve dependencies
+ */
+export class iocContainer {
+  // Stubbing register
+  public register(): void {}
+
+  // Stubbing resolve
+  public resolve(): void {}
+}
