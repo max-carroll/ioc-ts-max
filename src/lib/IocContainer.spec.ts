330d82e681567f1e09673e80a4160fba65e82243
Max Carroll <max.carroll@barbal.co>
2021-10-18 10:11:24 +0100

    assert interface may not be registered twice

--- src/lib/IocContainer.spec.ts
+++ src/lib/IocContainer.spec.ts
@@ -2,7 +2,7 @@ import test from 'ava'
 
 import { IocContainer } from './iocContainer'
 
-test('IocContainer', (t) => {
+test('IocContainer Resolves to an instance of the registered class', (t) => {
   const container = new IocContainer()
 
   interface IBird {
@@ -21,4 +21,25 @@ test('IocContainer', (t) => {
   const result = resolved.fly()
   const expected = 5
   t.is(result, expected)
+})
+
+test('IocContainer cannot register to the same interface twice', (t) => {
+  const container = new IocContainer()
+
+  interface IBird {
+    fly: () => number
+  }
+
+  class Eagle implements IBird {
+    public fly() {
+      return 5
+    }
+  }
+
+  container.register('IBird', Eagle)
+  const secondRegistration = () => container.register('IBird', Eagle)
+
+  const error = t.throws(secondRegistration, { instanceOf: Error }, 'expect second registration to return an error')
+
+  t.is(error.message, 'Cannot register the same interface twice')
 })
